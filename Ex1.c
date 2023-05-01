0fc9d536cc4f01e153a0dbd5a6ed3040b951965f
brubruna225 <131907463+brubruna225@users.noreply.github.com>
2023-04-30 22:53:44 -0300

    Add files via upload

---
+++ Ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+    float valueBem, valueDepre, valueBemDepre, DepreAcumu
+    int periodo;
+
+int main() {
+    printf("Informe valor a ser depreciado: ");
+    scanf("%f", &valueBem);
+
+    printf("Informe em anos a ser depreciado: ");
+    scanf("%i", &valueBemDepre);
+    
+    fffff
+
+    return 0;
+}
